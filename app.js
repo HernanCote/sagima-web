548a7b78f77503032bde96c350014a3024ce99b7
Hernan Cote <hernan.cote@zemoga.com>
2020-04-28 21:34:52 -0500

    added path as required reference

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphQLHttp = require('express-graphql');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const isAuth = require('./middleware/is-auth');
 
